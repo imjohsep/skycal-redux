5cdef4c6f1b6a75f7b94f5fce6201dae0b6fa85b
Joseph Watters <jwatters@jawbone.com>
2016-09-12 09:27:48 -0700

    Add endpoint for retrieving a month worth of grouped events

--- router.js
+++ router.js
@@ -81,4 +81,52 @@ module.exports = function (app) {
       res.send(groupedEvents)
     })
   })
+
+  // Grouped Events for a single Month
+  app.get('/api/events/grouped/:year/:month', function (req, res) {
+    var start_date = new Date(req.params.year, req.params.month)
+    var end_date = new Date(req.params.year, req.params.month + 1, 0)
+
+    Event.aggregate(
+      {
+        $match: {
+          occurrence_at: {$gte: start_date, $lte: end_date}
+        }
+      }, {
+        $group: {
+          _id: {
+            date: {
+              year: {$year: "$occurrence_at"},
+              month: {$month: "$occurrence_at"},
+              day: {$dayOfMonth: "$occurrence_at"}
+            },
+            occurrence_at: "$occurrence_at",
+            description: "$description",
+            uid: "$_id"
+          },
+        }
+      }, {
+        $sort: {
+          "_id.date.year": 1,
+          "_id.date.month": 1,
+          "_id.date.day": 1,
+        }
+    }
+    ).exec(function (err, events) {
+      if (err) res.send({'error': err})
+
+      var groupedEvents = {}
+      _.map(events, function(event) {
+        var key = event._id.date.year + '-' + event._id.date.month + '-' + event._id.date.day
+
+        if (groupedEvents[key] === undefined) {
+            groupedEvents[key] = []
+        }
+
+        groupedEvents[key].push(event._id)
+      })
+
+      res.send(groupedEvents)
+    })
+  })
 }
