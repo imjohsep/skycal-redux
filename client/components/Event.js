0fd4219d8c20f597361643b67dbfaba220eedbee
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2016-11-25 16:29:59 -0800

    updating homepage view

--- client/components/Event.js
+++ client/components/Event.js
@@ -1,14 +1,18 @@
 import React, {PropTypes, Component} from 'react'
-
+import { capitalize } from '../helpers';
 export default class Event extends Component {
   render () {
     return (
-      <div className='eventItem'>
-        {this.props.date} - {this.props.description}
+      <div className={'eventItem ' + 'eventImage--' + this.props.type}>
+      	<div className='eventItem-text'>
+        	<p>{this.props.date}</p>
+        	<p>{capitalize(this.props.description)}</p>
+        </div>
       </div>
     )
   }
 }
 
 Event.propTypes = { date: PropTypes.string.isRequired,
-                    description: PropTypes.string.isRequired }
+                    description: PropTypes.string.isRequired,
+                    type: PropTypes.string.isRequired }
